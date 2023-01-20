bad1c5119b367aea82f545ca51af6ec898f6455a
Anthony Rafidison <100955619+anthonyraf@users.noreply.github.com>
2023-01-20 08:20:16 +0000

    Add: Test for annotations check

--- speed/tests/test_utils.py
+++ speed/tests/test_utils.py
@@ -1,9 +1,11 @@
-from speed.utils import check_args, mergeable
-
 import pytest
+import inspect
+from speed.utils import check_args, mergeable, check_annotations
+
 
 def test_check_args():
     pass
+
 
 class TestMergeableDicts:
     def test_true(self):
@@ -17,5 +19,22 @@ class TestMergeableDicts:
         with pytest.raises(KeyError):
             mergeable(dict1, dict2)
 
+
 class TestStructArgs:
     pass
+
+
+class TestCheckAnnotations:
+    def test_valid(self):
+        def function_with_annotation(a: int, b: str, c: list, d: bool):
+            pass
+
+        assert check_annotations(
+            inspect.getfullargspec(function_with_annotation)) is None
+    
+    def test_no_annotation(self):
+        def function_without_annotation(a, b, c, d):
+            pass
+
+        with pytest.raises(TypeError):
+            check_annotations(inspect.getfullargspec(function_without_annotation))
