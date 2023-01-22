7fe8ad9ab36cc2efe900dbdecd9ab809a28ff83a
Anthony Rafidison <100955619+anthonyraf@users.noreply.github.com>
2023-01-22 09:09:10 +0000

    Add: is_{type} method for all custom types

--- speed/speed/utils/_types.py
+++ speed/speed/utils/_types.py
@@ -1,23 +1,32 @@
 
-class Bool:
+class MyType:
+    types = {"is_bool":"BOOL", "is_int":"INT", "is_str":"STR", "is_list":"LIST"}
+    def __init__(self):
+        for method_name in MyType.types:
+            setattr(self, method_name, self.check_data_type(MyType.types[method_name])) 
+
+    def check_data_type(self, data_type):
+        return lambda: hasattr(self, data_type)    
+
+class Bool(MyType):
     BOOL = 1 
     """Bool argument type class"""
     def __repr__(self) -> str:
         return f"[{bool.__name__}]"
 
-class Int(int):
+class Int(MyType, int):
     INT = 1
     """Int argument type class"""
     def __repr__(self) -> str:
         return f"[{int.__name__}]"
 
-class Str(str):
+class Str(MyType, str):
     STR = 1
     """Str argument type class"""
     def __repr__(self) -> str:
         return f"[{str.__name__}]"
 
-class List(list):
+class List(MyType,list):
     LIST = 1
     """List argument type class"""
     def __repr__(self) -> str:
@@ -38,6 +47,5 @@ def custom_type(obj):
         return Str()
     elif issubclass(obj, list):
         return List()
-
     else:
         raise TypeError("Argument must be a subclass of one of these class: int | str | list | bool")
